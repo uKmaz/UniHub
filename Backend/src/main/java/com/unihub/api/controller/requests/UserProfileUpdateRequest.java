bd8e832fb286e9655eac9d3613c493d71bb5d313
Emre Uçmaz <emre1563242a@gmail.com>
2025-07-31 17:56:16 +0300

    Last Update

--- Backend/src/main/java/com/unihub/api/controller/requests/UserProfileUpdateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.unihub.api.controller.requests;
-
-public class UserProfileUpdateRequest {
-    // Fields that the user is allowed to change
-    public String name;
-    public String surname;
-    public String profilePictureUrl;
-}
